bc9703ba7f2549ccf7f0c15888197f0122d859ed
unknown <quyenlevan97@gmail.com>
2022-10-18 23:35:40 +0700

    fix port to deploy heruku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connectDB = require('./server/database/connection')
 //
 const app = express();
 dotenv.config({ path: 'config.env' })
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000 ;
 
 //log request
 app.use(morgan('tiny'));
